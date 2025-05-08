5f755717fc216853a7b81a9fda772e302d2433a3
YlovexLN <1584544837@qq.com>
2025-05-09 01:15:10 +0800

    refactor(agent): 将自定义动作类重命名为自定义识别类并更新逻辑

--- agent/my_action.py
+++ agent/my_action.py
@@ -1,17 +1,37 @@
 from maa.agent.agent_server import AgentServer
-from maa.custom_action import CustomAction
+from maa.custom_recognition import CustomRecognition
 from maa.context import Context
 
 
-@AgentServer.custom_action("my_action_111")
-class MyCustomAction(CustomAction):
+@AgentServer.custom_recognition("my_reco_222")
+class MyRecongition(CustomRecognition):
 
-    def run(
+    def analyze(
         self,
         context: Context,
-        argv: CustomAction.RunArg,
-    ) -> bool:
+        argv: CustomRecognition.AnalyzeArg,
+    ) -> CustomRecognition.AnalyzeResult:
 
-        print("my_action_111 is running!")
+        reco_detail = context.run_recognition(
+            "MyCustomOCR",
+            argv.image,
+            pipeline_override={"MyCustomOCR": {"roi": [100, 100, 200, 300]}},
+        )
 
-        return True
+        # context is a reference, will override the pipeline for whole task
+        context.override_pipeline({"MyCustomOCR": {"roi": [1, 1, 114, 514]}})
+        # context.run_recognition ...
+
+        # make a new context to override the pipeline, only for itself
+        new_context = context.clone()
+        new_context.override_pipeline({"MyCustomOCR": {"roi": [100, 200, 300, 400]}})
+        reco_detail = new_context.run_recognition("MyCustomOCR", argv.image)
+
+        click_job = context.tasker.controller.post_click(10, 20)
+        click_job.wait()
+
+        context.override_next(argv.node_name, ["TaskA", "TaskB"])
+
+        return CustomRecognition.AnalyzeResult(
+            box=(0, 0, 100, 100), detail="Hello World!"
+        )
